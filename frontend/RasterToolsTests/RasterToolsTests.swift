d0fb058a775cb891408062b26b10b53a0f31be72
Paul Maček <242438+macek@users.noreply.github.com>
2026-03-20 20:04:52 -0300

    Initial Commit

---
+++ frontend/RasterToolsTests/RasterToolsTests.swift
@@ -0,0 +1,17 @@
+//
+//  RasterToolsTests.swift
+//  RasterToolsTests
+//
+//  Created by Marek on 2026-03-20.
+//
+
+import Testing
+@testable import RasterTools
+
+struct RasterToolsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
